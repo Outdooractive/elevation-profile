7dcd153e7245d29f6d34d706119f1eba20d5b390
Jens Thiele <karme@karme.de>
2013-03-14 16:56:03 +0100

    added python client

---
+++ python/dem.py
@@ -0,0 +1,79 @@
+import socket
+import re
+import string
+import time
+import sys
+
+from sexp import sexpReadFromString
+from sexp import sexpWriteToString
+
+def retry(f, count, onerrorf = lambda e: (sys.stderr.write("error:"+str(e)+"\n"), time.sleep(1))):
+    while True:
+        try:
+            return f()
+        except Exception as e:
+            try:
+                onerrorf(e)
+            except:
+                True
+            count=count-1
+            if not (count > 0):
+                raise
+
+def getFunctions(host, port):
+    # f=getUpsamplePL4D("localhost",2223)
+    # f([[8.5,48.5],[8.6,48.5]],1000)
+    # python scope or better binding sucks
+    # google for "UnboundLocalError: local variable" "referenced before assignment"
+    # http://mail.python.org/pipermail/python-list/2007-August/452164.html
+    f=[False]
+    assertProtocol = lambda rmaj,rmin: False
+
+    def connect2():
+        s=socket.create_connection((host,port))
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        r=s.makefile()
+        s.close()
+        return r
+
+    def connect():
+        f[0]=retry(connect2, 5)
+        assertProtocol(1,0)
+
+    def reconnect():
+        f[0].close()
+        connect()
+
+    def remoteEval2(x):
+        # sys.stderr.write(sexpWriteToString(x)+"\n")
+        f[0].write(sexpWriteToString(x)+"\n")
+        f[0].flush()
+        r=f[0].readline()
+        if r=='':
+            raise Exception("no result")
+        # sys.stderr.write("r='"+r+"'\n")
+        return r
+
+    def remoteEval(x):
+        return retry(lambda:remoteEval2(x), 5, lambda e: (sys.stderr.write("error:"+str(e)+"\n"), reconnect()))
+
+    def assertProtocol2(rmaj,rmin):
+        # sys.stderr.write(str(parseResult(remoteEval(["protocol-version"])))+"\n")
+        maj,min=parseResult(remoteEval(["protocol-version"]))
+        assert maj==rmaj
+        assert min==rmin
+
+    def parseResult(s):
+        assert not s.startswith('(error')
+        return sexpReadFromString(s)
+
+    def pl4d(pl,dist):
+        return parseResult(remoteEval(["upsample-polyline->4d","wgs84",pl,dist]))
+    
+    assertProtocol = assertProtocol2
+    connect()
+    return {"z":lambda pl: parseResult(remoteEval(["z"]+pl)),
+            "polyline->3d":lambda pl: parseResult(remoteEval(["polyline->3d",pl])),
+            "upsample-polyline->4d":lambda pl, dist: parseResult(remoteEval(["upsample-polyline->4d","wgs84",pl,dist])),
+            "sample-polyline->4d":lambda pl, count: parseResult(remoteEval(["sample-polyline->4d","wgs84",pl,count]))}
+            
---
+++ python/sexp.py
@@ -0,0 +1,59 @@
+# inspired by:
+# pyparsing example sexpParser.py
+# note: only using a small subset
+#
+# Demonstration of the pyparsing module, implementing a simple S-expression
+# parser.
+#
+# Updates:
+#  November, 2011 - fixed errors in precedence of alternatives in simpleString;
+#      fixed exception raised in verifyLen to properly signal the input string 
+#      and exception location so that markInputline works correctly; fixed 
+#      definition of decimal to accept a single '0' and optional leading '-'
+#      sign; updated tests to improve parser coverage
+#
+# Copyright 2007-2011, by Paul McGuire
+# Copyright (c) 2003,2004  Paul T. McGuire
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import pyparsing
+import string
+
+sexpReader = pyparsing.Forward()
+qstring = pyparsing.dblQuotedString.setParseAction(pyparsing.removeQuotes)
+decimal = pyparsing.Regex(r'-?0|[1-9]\d*').setParseAction(lambda t: int(t[0])) # maybe always return float?
+real = pyparsing.Regex(r"[+-]?\d+\.\d*([eE][+-]?\d+)?").setParseAction(lambda tokens: float(tokens[0]))
+literal = real | decimal | qstring
+sexpReader << ( literal | pyparsing.nestedExpr("(",")",sexpReader) )
+
+def sexpReadFromString(s):
+    return sexpReader.parseString(s).asList()[0]
+
+# todo: quick hack
+def sexpWriteToString(l):
+    if isinstance(l,type('')):
+        return l # todo: maybe quote?! but how to handle symbols then?!
+    elif isinstance(l,type(1)) or isinstance(l,type(1.1)):
+        return str(l)
+    elif isinstance(l,type([])):
+        return "("+string.join(map(lambda x: sexpWriteToString(x), l))+")"
+    else:
+        assert False
